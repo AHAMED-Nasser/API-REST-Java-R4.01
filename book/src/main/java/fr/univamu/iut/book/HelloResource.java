3a11ad23e7ef53b7083f5765bac6282139e4c0f8
AHAMED-Nasser <137123083+AHAMED-Nasser@users.noreply.github.com>
2026-03-26 15:03:51 +0100

    Connection à la base de donnée (#1)
    Co-authored-by: nularyNasser <nahamed.sveil@gmail.com>

--- book/src/main/java/fr/univamu/iut/book/HelloResource.java
+++ book/src/main/java/fr/univamu/iut/book/HelloResource.java
@@ -4,11 +4,20 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
+import java.sql.SQLException;
+
 @Path("/hello-world")
 public class HelloResource {
+
+    BookRepositoryMariadb db = new BookRepositoryMariadb("jdbc:mariadb://mysql-blog-td.alwaysdata.net/blog-td_library", "blog-td", "Rb.velocity+6");
+
+    public HelloResource() throws SQLException, ClassNotFoundException {
+    }
+
+
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World!";
+        return db.getAllBooks().toString();
     }
 }
