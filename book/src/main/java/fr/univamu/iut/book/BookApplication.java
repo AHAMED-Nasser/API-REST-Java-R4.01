79737eb6d7ba941ef89ca7fbeac6f18e02089f86
nularyNasser <nahamed.sveil@gmail.com>
2026-03-30 16:19:49 +0200

    ajout de la méthode getSingleton

--- book/src/main/java/fr/univamu/iut/book/BookApplication.java
+++ book/src/main/java/fr/univamu/iut/book/BookApplication.java
@@ -7,6 +7,9 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/api")
 @ApplicationScoped
 public class BookApplication extends Application {
@@ -16,6 +19,27 @@ public class BookApplication extends Application {
     String db_url = dotenv.get("DB_URL");
     String db_user = dotenv.get("DB_USER");
     String db_password = dotenv.get("DB_PASSWORD");
+
+    @Override
+    public Set<Object> getSingletons() {
+        Set<Object> set = new HashSet<>();
+
+        // Création de la connection à la base de données et initialisation du service associé
+        BookService service = null ;
+        try{
+            BookRepositoryMariadb db = new BookRepositoryMariadb(db_url, db_user, db_password);
+            service = new BookService(db);
+        }
+        catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+
+        // Création de la ressource en lui passant paramètre les services à exécuter en fonction
+        // des différents endpoints proposés (i.e. requêtes HTTP acceptées)
+        set.add(new BookRessource(service));
+
+        return set;
+    }
 
     /**
      * Méthode appelée par l'API CDI pour injecter la connection à la base de données au moment de la création
